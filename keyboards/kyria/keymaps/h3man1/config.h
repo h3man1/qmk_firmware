10ae9a576aa533d6688c52bc4a3cc6f201abd6ee
h3man1 <h3man@outlook.com>
2020-10-10 23:13:47 +0200

    second commit

--- keyboards/kyria/keymaps/h3man1/config.h
+++ keyboards/kyria/keymaps/h3man1/config.h
@@ -14,6 +14,9 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#define MASTER_RIGHT
+
+
 #pragma once
 
 #ifdef OLED_DRIVER_ENABLE
@@ -22,12 +25,47 @@
 
 #ifdef RGBLIGHT_ENABLE
   #define RGBLIGHT_LAYERS
-  #define RGBLIGHT_ANIMATIONS
+  //#define RGBLIGHT_ANIMATIONS
   #define RGBLIGHT_HUE_STEP 8
   #define RGBLIGHT_SAT_STEP 8
   #define RGBLIGHT_VAL_STEP 8
+  #define RGBLIGHT_SLEEP
+  #define RGBLIGHT_SPLIT
 #endif
 
 // If you are using an Elite C rev3 on the slave side, uncomment the lines below:
- #define SPLIT_USB_DETECT
- #define NO_USB_STARTUP_CHECK
+#define SPLIT_USB_DETECT
+#define NO_USB_STARTUP_CHECK
+
+
+
+
+
+
+//Settings for Tap Dance
+
+
+// to make layer and mod usage more snappy slightly lower Tapping term from the 200ms default 
+#define TAPPING_TERM 200
+
+// Prevent normal rollover on alphas from accidentally triggering mods.
+#define IGNORE_MOD_TAP_INTERRUPT
+
+// This prevents accidental repeats of the tap-hold keys when typing quickly.
+#define TAPPING_FORCE_HOLD
+
+
+
+
+#define NO_ACTION_MACRO
+#define NO_ACTION_FUNCTION
+#define NO_ACTION_ONESHOT
+
+
+#ifdef RGBLIGHT_ENABLE
+    #define RGBLIGHT_LED_MAP {0,1,2,9,8,7,4,3,5,6,19,18,17,10,11,12,15,16,14,13} // Orients Kyria LEDs to a circle around both halves.
+    //#define RBGLIGHT_LED_MAP {9,8,6,7,5,3,2,4,1,0,10,12,13,11,14,16,17,15,18,19} // Orients Kyria LEDs for a left half-right half columnar progression.
+    #define RGBLIGHT_EFFECT_RAINBOW_SWIRL
+    //#define RGBLIGHT_EFFECT_STATIC_GRADIENT
+    #define RGBLIGHT_SLEEP
+#endif
